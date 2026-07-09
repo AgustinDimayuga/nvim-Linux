9f519684897bd7dfbdfd4de6b185ba9014291100
AgustinDimayuga <axocuadi@calpoly.edu>
2026-07-08 20:15:20 -0700

    none-ls

--- lua/plugins/none-ls.lua
+++ lua/plugins/none-ls.lua
@@ -7,10 +7,10 @@ return {
     local null_ls = require("null-ls")
     null_ls.setup({
       sources = {
-        -- null_ls.builtins.formatting.stylua.with({
-        --   command = "/run/current-system/sw/bin/stylua",
-        -- }),
-        null_ls.builtins.formatting.black,    -- python
+        null_ls.builtins.formatting.stylua.with({
+          command = "/run/current-system/sw/bin/stylua",
+        }),
+        null_ls.builtins.formatting.black, -- python
 
         null_ls.builtins.formatting.prettier, -- javascript html md
         null_ls.builtins.formatting.nixpkgs_fmt,
